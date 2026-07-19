a4f4129b8ad8d897bb7153dce9b827158e824f53
VyacheslavKokorin <kokorin17@gmail.com>
2026-07-19 08:23:23 +0300

    Добавлено тестовое путешествие

--- db/init.sql
+++ db/init.sql
@@ -33,3 +33,39 @@ VALUES (
     'traveler@travel.ru',
     '$2b$10$TruKkVmHn5HofKEZ0WmUsOGIeDMHHNX8oZBOyO6Y5Jdm2bzn90yF6'
 );
+
+-- Тестовое путешествие для просмотра записей других пользователей
+INSERT INTO trips (
+    user_id,
+    title,
+    country,
+    city,
+    description,
+    start_date,
+    end_date,
+    latitude,
+    longitude,
+    image_url,
+    cost
+)
+SELECT
+    users.id,
+    'Поездка в Санкт-Петербург',
+    'Россия',
+    'Санкт-Петербург',
+    'Несколько дней гулял по центру Санкт-Петербурга. Посетил Дворцовую площадь, Эрмитаж, Невский проспект и набережную Невы.',
+    '2026-05-10',
+    '2026-05-14',
+    59.9343,
+    30.3351,
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Palace_Square_in_St._Petersburg.jpg/1280px-Palace_Square_in_St._Petersburg.jpg',
+    35000
+FROM users
+WHERE users.email = 'traveler@travel.ru'
+  AND NOT EXISTS (
+      SELECT 1
+      FROM trips
+      WHERE trips.user_id = users.id
+        AND trips.title = 'Поездка в Санкт-Петербург'
+        AND trips.start_date = '2026-05-10'
+  );
